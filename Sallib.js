9700a9e6f07a39f53395fd90d9c47f2904ca9161
Gaspard <47181975+GaspardLauras@users.noreply.github.com>
2020-01-05 19:39:54 +0100

    Ajout de l'affichage de salles pour l'épi1

--- Sallib.js
+++ Sallib.js
@@ -3,47 +3,58 @@
 
 //_________________________________________
 //Salles libres
-
-
-
-
 var arraySalles = new Array(1,2,3,4,5,6);
 
-for(var i = 0; i < 6; i++){
-  $('div.salles_libres').append("<div class='salles_libres_div'>Salles numéro "+
+for(var i = 0; i < arraySalles.length; i++){ //ajout de toutes les "salles" dans la balise '<div class="salles_libres"/>'
+  $('div.salles_libres').append("<div class='salles_libres_salles_div'>Salles numéro "+
     arraySalles[i]+
-    "</div> \n");
+    "</div>");
 }
 
+//Mise en page des div de salles libres
+$('div.salles_libres_salles_div').css('height','30px');
+$('div.salles_libres_salles_div').css('width','200px');
+$('div.salles_libres_salles_div').css('margin','0 auto');
+$('div.salles_libres_salles_div').css('border','2px solid black');
+$('div.salles_libres_salles_div').css('margin-bottom','10px');
 
-$('div.salles_libres_div').css('height','30px');
-$('div.salles_libres_div').css('width','200px');
-$('div.salles_libres_div').css('margin','0 auto');
-$('div.salles_libres_div').css('border','2px solid black');
-$('div.salles_libres_div').css('margin-bottom','10px');
 
 
+$('div.salles_libres_salles_div').hide();//salles libres cachées par défaut
 
-$('div.salles_libres_div').hide();
-
-$('div.salles_libres p').click(function(){
-  $('div.salles_libres_div').toggle(300);
+$('div.salles_libres').click(function(){
+  $('div.salles_libres_salles_div').toggle(300);//Permet de cacher et d'afficher les salles libres
+  //Le paramètre est le temps que met le navigateur à afficher ou cacher en ms
 });
 
 
 
 //_________________________________________
 //Carte
+var arraySallesEpi1 = new Array(1051,1052,1053,1054,1055,1056);
 
+for(var i = 0; i < arraySallesEpi1.length; i++){ //ajout de toutes les "salles" dans la balise '<div class="salles_libres"/>'
+  $('div#epi_1').append("<div class='salles_carte_epi1'>"+
+  	arraySallesEpi1[i]
+  	+"</div>");
+}
+
+$('div.salles_carte_epi1').hide();
 $('div#epi_1').click(function(){
-  $('div#epi_1').css('width','500px');
-  $('div#epi_1').css('height','500px');
-  $('div#epi_1').click(function(){
-  	$('div#epi_1').css('width','250px');
-  	$('div#epi_1').css('height','');
-  });
+  $('div.salles_carte_epi1').toggle(300);//Permet de cacher et d'afficher les salles libres
+  //Le paramètre est le temps que met le navigateur à afficher ou cacher en ms
 });
 
+$('div.salles_carte_epi1').css('height','30px');
+$('div.salles_carte_epi1').css('width','200px');
+$('div.salles_carte_epi1').css('margin','0 auto');
+$('div.salles_carte_epi1').css('border','2px solid black');
+$('div.salles_carte_epi1').css('margin-bottom','10px');
+
+
+
+//______________________________
+//Cette partie sera à virer et à remplacer par la méthode ci-dessus
 $('div#epi_2').click(function(){
   $('div#epi_2').css('width','500px');
   $('div#epi_2').css('height','500px');
@@ -57,7 +68,7 @@ $('div#epi_3').click(function(){
   $('div#epi_3').css('width','500px');
   $('div#epi_3').css('height','500px');
   $('div#epi_3').click(function(){
-  	$('div#epi_3').css('width','300px');
+  	$('div#epi_3').css('width','250px');
   	$('div#epi_3').css('height','');
   });
 });
@@ -66,7 +77,7 @@ $('div#epi_4').click(function(){
   $('div#epi_4').css('width','500px');
   $('div#epi_4').css('height','500px');
   $('div#epi_4').click(function(){
-  	$('div#epi_4').css('width','300px');
+  	$('div#epi_4').css('width','250px');
   	$('div#epi_4').css('height','');
   });
 });
@@ -75,7 +86,7 @@ $('div#epi_5').click(function(){
   $('div#epi_5').css('width','500px');
   $('div#epi_5').css('height','500px');
   $('div#epi_5').click(function(){
-  	$('div#epi_5').css('width','300px');
+  	$('div#epi_5').css('width','250px');
   	$('div#epi_5').css('height','');
   });
 });
@@ -84,7 +95,7 @@ $('div#epi_6').click(function(){
   $('div#epi_6').css('width','500px');
   $('div#epi_6').css('height','500px');
   $('div#epi_6').click(function(){
-  	$('div#epi_6').css('width','300px');
+  	$('div#epi_6').css('width','250px');
   	$('div#epi_6').css('height','');
   });
 });
