c3c64115834c53c47d8181c446863274b9e4371f
Gaspard Lauras <gaspard.lauras@edu.esiee.fr>
2020-01-09 22:05:01 +0100

    Ajout d'une fonction d'ajout des salles dans les balises HTML

--- Sallib.js
+++ Sallib.js
@@ -1,24 +1,31 @@
+function ajoutSalles(arraySalles, classDivCree, attrDivCible){
+
+  console.log(classDivCree+"-->"+attrDivCible);
+
+  for(var i = 0; i < arraySalles.length; i++){ //ajout de toutes les "salles" dans la balise '<div class="salles_libres"/>'
+    $('div'+attrDivCible).append("<div class='"+ classDivCree +"'>"+
+      arraySallesLibres[i]+
+      "</div>");
+    console.log(arraySalles[i]);
+  }
+
+  //Mise en page des div de salles libres
+  $('div.'+classDivCree).css('height','30px');
+  $('div.'+classDivCree).css('width','200px');
+  $('div.'+classDivCree).css('margin','0 auto');
+  $('div.'+classDivCree).css('border','2px solid black');
+  $('div.'+classDivCree).css('margin-bottom','10px');
+
+}
+
 //Menu
 
 
 //_________________________________________
 //Salles libres
-var arraySalles = new Array(1,2,3,4,5,6);
+var arraySallesLibres = new Array(2305,5051,3207,4405,1104);
 
-for(var i = 0; i < arraySalles.length; i++){ //ajout de toutes les "salles" dans la balise '<div class="salles_libres"/>'
-  $('div.salles_libres').append("<div class='salles_libres_salles_div'>Salles numéro "+
-    arraySalles[i]+
-    "</div>");
-}
-
-//Mise en page des div de salles libres
-$('div.salles_libres_salles_div').css('height','30px');
-$('div.salles_libres_salles_div').css('width','200px');
-$('div.salles_libres_salles_div').css('margin','0 auto');
-$('div.salles_libres_salles_div').css('border','2px solid black');
-$('div.salles_libres_salles_div').css('margin-bottom','10px');
-
-
+ajoutSalles(arraySallesLibres,'salles_libres_salles_div','.salles_libres');
 
 $('div.salles_libres_salles_div').hide();//salles libres cachées par défaut
 
@@ -31,6 +38,32 @@ $('div.salles_libres').click(function(){
 
 //_________________________________________
 //Carte
+
+//-------
+//RUE
+var arraySallesRue = new Array(110,210,160,260);
+
+ajoutSalles(arraySallesRue,'salles_carte_rue','.salles_rue_container');
+
+// for(var i = 0; i < arraySallesRue.length; i++){
+//   $('div.salles_rue_container').append("<div class='salles_carte_rue'>"+
+//   arraySallesRue[i]+
+//   "</div>");
+// }
+
+//$('div.salles_carte_rue').hide();
+$('div.rue').click(function(){
+  $('div.salles_carte_rue').toggle(300);//Permet de cacher et d'afficher les salles libres
+  //Le paramètre est le temps que met le navigateur à afficher ou cacher en ms
+});
+
+// $('div.salles_carte_rue').css('height','30px');
+// $('div.salles_carte_rue').css('width','200px');
+// $('div.salles_carte_rue').css('margin','0 auto');
+// $('div.salles_carte_rue').css('border','2px solid black');
+// $('div.salles_carte_rue').css('margin-bottom','10px');
+
+
 //-------
 //EPI1
 var arraySallesEpi1 = new Array(1051,1052,1053,1054,1055,1056);
@@ -59,7 +92,7 @@ $('div.salles_carte_epi1').css('margin-bottom','10px');
 //EPI2
 var arraySallesEpi2 = new Array(2051,2052,2053,2054,2055,2056);
 
-for(var i = 0; i < arraySallesEpi1.length; i++){ //ajout de toutes les "salles" dans la balise '<div class="salles_libres"/>'
+for(var i = 0; i < arraySallesEpi2.length; i++){ //ajout de toutes les "salles" dans la balise '<div class="salles_libres"/>'
   $('div#epi_2').append("<div class='salles_carte_epi2'>"+
   	arraySallesEpi2[i]
   	+"</div>");
