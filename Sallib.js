fc0c8e85171086aa54575043a21e9ce3a3248b42
Gaspard Lauras <gaspard.lauras@edu.esiee.fr>
2020-01-09 14:28:56 +0100

    Test d'expansion des balises de la carte

--- Sallib.js
+++ Sallib.js
@@ -3,58 +3,89 @@
 
 //_________________________________________
 //Salles libres
-
 var arraySalles = new Array(1,2,3,4,5,6);
 
-for(var i = 0; i < 6; i++){
-  $('div.salles_libres').append("<div class='salles_libres_div'>Salles numéro "+
+for(var i = 0; i < arraySalles.length; i++){ //ajout de toutes les "salles" dans la balise '<div class="salles_libres"/>'
+  $('div.salles_libres').append("<div class='salles_libres_salles_div'>Salles numéro "+
     arraySalles[i]+
-    "</div> \n");
+    "</div>");
 }
 
-$('div.salles_libres_div').css('border','2px solid black');
-$('div.salles_libres_div').css('height','30px');
-// $('div.salles_libres_div').css('width','200px');
+//Mise en page des div de salles libres
+$('div.salles_libres_salles_div').css('height','30px');
+$('div.salles_libres_salles_div').css('width','200px');
+$('div.salles_libres_salles_div').css('margin','0 auto');
+$('div.salles_libres_salles_div').css('border','2px solid black');
+$('div.salles_libres_salles_div').css('margin-bottom','10px');
 
-// $('div.salles_libres_div').css('vertical-align','middle');
-
-// $('div.salles_libres_div').css('margin-bottom','10px');
 
 
-// $('div.salles_libres_div').hide();
+$('div.salles_libres_salles_div').hide();//salles libres cachées par défaut
 
-// $('div.salles_libres p').click(function(){
-//   $('div.salles_libres_div').toggle(300);
-// });
+$('div.salles_libres').click(function(){
+  $('div.salles_libres_salles_div').toggle(300);//Permet de cacher et d'afficher les salles libres
+  //Le paramètre est le temps que met le navigateur à afficher ou cacher en ms
+});
 
 
 
 //_________________________________________
 //Carte
 
+//-------
+//EPI1
+var arraySallesEpi1 = new Array(1051,1052,1053,1054,1055,1056);
+
+for(var i = 0; i < arraySallesEpi1.length; i++){ //ajout de toutes les "salles" dans la balise '<div class="salles_libres"/>'
+  $('div#epi_1').append("<div class='salles_carte_epi1'>"+
+  	arraySallesEpi1[i]
+  	+"</div>");
+}
+
+$('div.salles_carte_epi1').hide();
 $('div#epi_1').click(function(){
-  $('div#epi_1').css('width','500px');
-  $('div#epi_1').css('height','500px');
-  $('div#epi_1').click(function(){
-  	$('div#epi_1').css('width','250px');
-  	$('div#epi_1').css('height','');
-  });
+  $('div.salles_carte_epi1').toggle(300);//Permet de cacher et d'afficher les salles libres
+  //Le paramètre est le temps que met le navigateur à afficher ou cacher en ms
 });
 
+$('div.salles_carte_epi1').css('height','30px');
+$('div.salles_carte_epi1').css('width','200px');
+$('div.salles_carte_epi1').css('margin','0 auto');
+$('div.salles_carte_epi1').css('border','2px solid black');
+$('div.salles_carte_epi1').css('margin-bottom','10px');
+
+//-------
+//EPI2
+var arraySallesEpi2 = new Array(2051,2052,2053,2054,2055,2056);
+
+for(var i = 0; i < arraySallesEpi1.length; i++){ //ajout de toutes les "salles" dans la balise '<div class="salles_libres"/>'
+  $('div#epi_2').append("<div class='salles_carte_epi2'>"+
+  	arraySallesEpi2[i]
+  	+"</div>");
+}
+
+$('div.salles_carte_epi2').hide();
 $('div#epi_2').click(function(){
-  $('div#epi_2').css('width','500px');
-  $('div#epi_2').css('height','500px');
-  $('div#epi_2').click(function(){
-  	$('div#epi_2').css('width','250px');
-  	$('div#epi_2').css('height','');
-  });
+  $('div.salles_carte_epi2').toggle(300);//Permet de cacher et d'afficher les salles libres
+  //Le paramètre est le temps que met le navigateur à afficher ou cacher en ms
 });
+
+$('div.salles_carte_epi2').css('height','30px');
+$('div.salles_carte_epi2').css('width','200px');
+$('div.salles_carte_epi2').css('margin','0 auto');
+$('div.salles_carte_epi2').css('border','2px solid black');
+$('div.salles_carte_epi2').css('margin-bottom','10px');
+
+
+
+//______________________________
+//Cette partie sera à virer et à remplacer par la méthode ci-dessus
 
 $('div#epi_3').click(function(){
   $('div#epi_3').css('width','500px');
   $('div#epi_3').css('height','500px');
   $('div#epi_3').click(function(){
-  	$('div#epi_3').css('width','300px');
+  	$('div#epi_3').css('width','250px');
   	$('div#epi_3').css('height','');
   });
 });
@@ -63,7 +94,7 @@ $('div#epi_4').click(function(){
   $('div#epi_4').css('width','500px');
   $('div#epi_4').css('height','500px');
   $('div#epi_4').click(function(){
-  	$('div#epi_4').css('width','300px');
+  	$('div#epi_4').css('width','250px');
   	$('div#epi_4').css('height','');
   });
 });
@@ -72,7 +103,7 @@ $('div#epi_5').click(function(){
   $('div#epi_5').css('width','500px');
   $('div#epi_5').css('height','500px');
   $('div#epi_5').click(function(){
-  	$('div#epi_5').css('width','300px');
+  	$('div#epi_5').css('width','250px');
   	$('div#epi_5').css('height','');
   });
 });
@@ -81,7 +112,7 @@ $('div#epi_6').click(function(){
   $('div#epi_6').css('width','500px');
   $('div#epi_6').css('height','500px');
   $('div#epi_6').click(function(){
-  	$('div#epi_6').css('width','300px');
+  	$('div#epi_6').css('width','250px');
   	$('div#epi_6').css('height','');
   });
 });
