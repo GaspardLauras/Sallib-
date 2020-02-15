ffce67acf122248ba8284b1ba3e4650ad0ae5876
Gaspard <47181975+GaspardLauras@users.noreply.github.com>
2020-02-15 16:57:29 +0100

    Ajout de l'affichages salles occupées

--- Sallib.js
+++ Sallib.js
@@ -16,9 +16,6 @@ function compare(Salles_possibles, salles_occupées){
   return SL;
 }
 
-
-
-
 //Permet d'ajouter les salles d'une liste
 function ajoutSallesH(arraySalles, classDivCree, attrDivCible){
   for(var i = 0; i < arraySalles.length; i++){ //ajout de toutes les 'salles' dans la balise '<div class='salles_libres'/>'
@@ -36,6 +33,12 @@ function ajoutSallesH(arraySalles, classDivCree, attrDivCible){
   $('div.'+classDivCree).css('margin','0 auto');
   $('div.'+classDivCree).css('margin-bottom','10px');
 }
+
+
+
+
+
+
 
 
 
@@ -94,9 +97,6 @@ function setErrorLog (functionId, errorMessage, duration){
 
 
 
-
-
-
 /*
 ====================================================================================================================================
                             Récupération date du jour - format MM/JJ/AAAA
@@ -107,12 +107,6 @@ function getCurrentDateForEvent(){
     let currentD = new Date();  
     return ((currentD.getMonth()+1)<10?"0":"") + (currentD.getMonth()+1) + "/" + (currentD.getDate()<10?"0":"") + currentD.getDate() + "/" + currentD.getFullYear();
 }
-
-
-
-
-
-
 
 
 
@@ -268,15 +262,6 @@ function occupiedRoomsPerEpi(occupiedRoomsList) {
 
 
 
-
-
-
-
-
-
-
-
-
 /*
 ====================================================================================================================================
                             Requêtes http
@@ -391,8 +376,6 @@ function getEvents(sessionId){
           }
         }
 
-        //console.log(occupiedRooms);
-
         // Pré-filtrage des salles :
         var processedList = filter(occupiedRooms);
         // console.log(processedList);
@@ -402,16 +385,6 @@ function getEvents(sessionId){
         console.log('OCCUPIED ROOMS:')
         //console.log(occupiedRooms);
         // utilisable en faisant occupiedRooms.epi1 etc.
-
-
-
-
-
-
-
-
-
-        //function ajoutSallesH(arraySalles, classDivCree, attrDivCible){
 
         occupiedRooms = occupiedRooms[0];
         console.log(occupiedRooms);
@@ -430,9 +403,6 @@ function getEvents(sessionId){
           $('div.salles_libres_div').toggle(300);//Permet de cacher et d'afficher les salles libres
           //Le paramètre est le temps que met le navigateur à afficher ou cacher en ms
         });
-
-
-        /////////////
 
         ajoutSallesH(occupiedRooms.epi1, 'salles_libres_epi1', '#epi_1');
         $('div.salles_libres_epi1').hide();//salles libres cachées par défaut
@@ -590,38 +560,13 @@ function ADEconnect (){
 
 }
 
-/*
-====================================================================================================================================
-                            Main
-====================================================================================================================================
-*/
-// Liste des salles par épi :   
-const EPI_1 = ['1001V', '1007V+', '1005', '1007V', '1051', '1055V+', '1103', '1105', '1107V+', '1109V+', '1201+', '1205V+', '1207V+', '1209V+', '1301+', '1305', '1307', '1309', '1401', '1403', '1405', '1407', '1409', '1452', '1456'];
-const EPI_2 = ['2101', '2102', '2103', '2104', '2105','2107', '2105', '2107', '2108', '2201+', '2205', '2207', '2209', '2305V+','2309+','2401V+','2409V+'];  
-const EPI_3 = ['3001', '3005', '3007', '3051', '3053', '3055', '3103', '3105', '3107', '3109', '3201', '3203', '3207+', '3301V+', '3305', '3307+', '3401V+', '3407V'];
-const EPI_4 = ['4003V+', '4007V', '4105V', '4109', '4201+', '4305', '4307+', '4401', '4403'];
-const EPI_5 = ['5004','5006V+','5008V++','5101+','5103+', '5105V', '5107V', '5155', '5201V+', '5207+', '5209V', '5301V','5308', '5309V', '5401V', '5403V','5407V'];
-const EPI_6 = ['6301','6401+','6409V'];
-const AUTRES = ['0110', '0210', '0160', '0260', '0112', '0113', '0114', '0115', '0162V', '0163', '0164V','0165V','0232', '0244', '0351'];
 
-// Liste salles de Cergy :
-const Cergy = ['108', '109', '110', '204', '208', 'B5', 'B6', 'C1', 'C2', 'C3', 'C4', 'P1', 'P2', 'P3', 'TP1', 'TP2', 'TP3', 'TP4', 'TPR1', 'TPR2'];
-// Liste des élements indésirables (salle à préciser, MD, salles de partiels):
-const Indesirables = ['salle à préciser 1', 'M.D.', '4405', '4405-2', '4405-3', '4405-4', , '4405-5', '4451','4451-2','4451-3', '7100'];
-// Elaboration du fitre qui va pré-traiter les données remontées :
-const Filter_default = Cergy.concat(Indesirables);
 
-// Paramètres pour les requêtes :
-const ip = 'planif.esiee.fr';
-const port ='8443'; 
-// URL commune aux requêtes :
-const baseURL = 'https://'+ ip +':'+ port +'/jsp/webapi';
 
-// API that enables cross-origin requests to anywhere :
-const proxyUrl = 'https://cors-anywhere.herokuapp.com/';  // Trouver une solution viable ! + CORS demander à D.B.
 
-// Connexion à ADE (main) :
-ADEconnect();
+
+
+
 
 
 /*
@@ -639,11 +584,6 @@ function getTime (currentD){
     return (currentD.getHours()<10?"0":"") + currentD.getHours()+ ":" + (currentD.getMinutes()<10?"0":"") + currentD.getMinutes()+ ":" + (currentD.getSeconds()<10?"0":"") + currentD.getSeconds();
 }
 
-<<<<<<< HEAD
-
-
-
-=======
 // Journalisation des requêtes
 function setLog (functionId, params, duration, size){
     // Initialisation objet temporel :
@@ -657,25 +597,12 @@ function setLog (functionId, params, duration, size){
         logger = logger + (debut==false?", ": "") + key + "="  + value;
         if (debut==true) debut=false;
     }
->>>>>>> 0bfcd3b645b8c97a9895a948b18b41300dbd3f50
-
     // Ajout taille de la réponse et durée de la requête :
     logger = logger + "} " + "| Size {" + size + " items}" + " | Duration {" + duration + "ms}";
     // Journaliser :
     return logger;
 }
 
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
-
-=======
 // Journalisation des erreurs
 function setErrorLog (functionId, errorMessage, duration){
     // Initialisation objet temporel :
@@ -685,6 +612,14 @@ function setErrorLog (functionId, errorMessage, duration){
     // Renvoyer le message d'erreur :
     return logger + errorMessage;  
 }
+
+
+
+
+
+
+
+
 
 
 /*
@@ -697,6 +632,15 @@ function getCurrentDateForEvent(){
     let currentD = new Date();  
     return ((currentD.getMonth()+1)<10?"0":"") + (currentD.getMonth()+1) + "/" + (currentD.getDate()<10?"0":"") + currentD.getDate() + "/" + currentD.getFullYear();
 }
+
+
+
+
+
+
+
+
+
 
 /*
 ====================================================================================================================================
@@ -833,6 +777,15 @@ function occupiedRoomsPerEpi(occupiedRoomsList) {
     return [{epi1 : occupiedRooms_epi1, epi2 : occupiedRooms_epi2, epi3 :  occupiedRooms_epi3, epi4 :  occupiedRooms_epi4, epi5 : occupiedRooms_epi5, epi6 :  occupiedRooms_epi6, autres : occupiedRooms_autres}];
 }
 
+
+
+
+
+
+
+
+
+
 /*
 ====================================================================================================================================
                             Requêtes http
@@ -902,63 +855,6 @@ function settingProject(sessionId, projectId){
         console.log(e);           // A logger avec events du pdf + avec heure etc.
         return e;
     });	
-}
-
-// Récupérer les events de ADE 
-function getEvents(sessionId){
-    var date = '02/24/2020';
-    // var date = getCurrentDateForEvent();
-    const events = baseURL + '?sessionId='+ sessionId +'&function=getEvents&tree=true&date=' + date + '&detail=8';
-    // Requête http :
-    var startRequest3 = new Date().getTime(); 
-    fetch(proxyUrl + events)
-        .then(response=>response.text()) 
-        .then(data => {
-            // Parser réponse en XML :
-            let parser = new DOMParser();	
-            let xmlResponse = parser.parseFromString(data, "application/xml"); 
-            // Log :
-            console.log(xmlResponse);
-            var endRequest3 = new Date().getTime();
-            var requestDuration3 = endRequest3 - startRequest3;
-            console.log(setLog('get events', [['Session ID', sessionId], ['tree', true], ['detail', 8], ['date', date]], requestDuration3, data.length)); 
-            // Récupération des balises <events> :
-            let event  = xmlResponse.getElementsByTagName('event');
-            // Récupération des balises inférieures <resources> :
-            var resources  = xmlResponse.getElementsByTagName('resources');
-            // Array de 3 valeurs par élément pour répertorier chaque salle avec ses dates de réservation programmé (https://stackoverflow.com/questions/13219348/adding-multiple-values-to-an-array-in-javascript)
-            var occupiedRooms = [];
-            // Parcourir chaque event de la trame xml retournée :
-            for(j=0; j < event.length; j++){
-                // Date de début de réservation de salle :
-                let startHour = event[j].getAttribute('startHour');
-                // Date de fin de réservation de salle :
-                let endHour = event[j].getAttribute('endHour');   
-                // Récupère les éléments fils de <resources> :
-                var childNodesResources = resources[j].children;
-                // Parcourir les balises <resources> :
-                for(i=0; i< childNodesResources.length; i++){
-                    // Parcourir les balises <resource> - tester s'il s'agit d'une ressource de type 'salle' :
-                    if(childNodesResources[i].getAttribute('category') == 'classroom'){
-                        // Récupérer le nom de la salle :
-                        occupiedRooms.push({startH : startHour, endH : endHour, room : childNodesResources[i].getAttribute('name')});
-                    }
-                }
-            }
-
-            // Pré-filtrage des salles :
-            var processedList = filter(occupiedRooms);
-            // Filtrage par épi :
-            var occupiedRooms = occupiedRoomsPerEpi(processedList);
-            console.log(occupiedRooms);   // utilisable en faisant occupiedRooms.epi1 etc.// utilisable en faisant occupiedRooms.epi1 etc.
-
-            // Déconnexion de session ADE :
-            disconnection(sessionId);
-        })
-        .catch(e => {
-            console.log(e);           // A logger avec events du pdf + avec heure etc.
-            return e;
-        });	
 }
 
 // Déconnexion de session ADE
@@ -1057,6 +953,15 @@ function ADEconnect (){
 
 }
 
+
+
+
+
+
+
+
+
+
 /*
 ====================================================================================================================================
                             Main
@@ -1089,4 +994,4 @@ const proxyUrl = 'https://cors-anywhere.herokuapp.com/';  // Trouver une solutio
 
 // Connexion à ADE (main) :
 ADEconnect();
->>>>>>> 0bfcd3b645b8c97a9895a948b18b41300dbd3f50
+
